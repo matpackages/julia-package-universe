7c7acfd74dacb5f47f8ff968785cfcefecae0d5a
Patrick <patrsc@nubu.at>
2023-03-11 18:15:56 +0100

    Add functions to convert.py

--- convert.py
+++ convert.py
@@ -1,15 +1,92 @@
 import sys
 import os
 import toml
+import json
+from version_spec import VersionSpec
 
 
 def main():
     registry_path = 'General'
     output_file = sys.argv[1]
     registry_file = os.path.join(registry_path, 'Registry.toml')
-    with open(registry_file, 'r') as f:
-        registry_data = toml.load(f)
-    print('done')
+    ignore = ['julia']
+    paths = read_package_paths(registry_file, ignore)
+    packages = {}
+    for name, path in paths.items():
+        packages[name] = read_package_data(os.path.join(registry_path, path))
+    write_json(output_file, packages)
+
+
+def read_package_paths(registry_file, ignore):
+    registry_data = read_toml(registry_file)
+    packages = registry_data['packages']
+    paths = {}
+    for val in packages.values():
+        name = val['name']
+        if name not in ignore:
+            path = val['path']
+            paths[name] = path
+    return paths
+
+
+def read_package_data(path):
+    deps_file = os.path.join(path, 'Compat.toml')
+    versions_file = os.path.join(path, 'Versions.toml')
+    deps = read_toml(deps_file)
+    deps = convert_dependencies(deps)
+    versions = read_versions(versions_file)
+    data = {}
+    for version in versions:
+        data[version] = get_dependencies(deps, version)
+    return data
+
+
+def read_versions(file):
+    data = read_toml(file)
+    versions = []
+    for v in data.keys():
+        ver = v
+        for char in ['-', '+']:
+            if char in ver:
+                p = ver.index(char)
+                ver = ver[0:p]
+        versions.append(ver)
+    versions = list(set(versions))
+    versions.sort(key=lambda s: list(map(int, s.split('.'))))
+    return versions
+
+
+def convert_dependencies(deps):
+    d = {}
+    for range, dependencies in deps.items():
+        r = VersionSpec.parse(range)
+        dep = {}
+        for name, spec in dependencies.items():
+            dep[name] = str(VersionSpec.parse(spec))
+        d[r] = dep
+    return d
+
+
+def get_dependencies(deps, version):
+    d = {}
+    for range, dependencies in deps.items():
+        if version in range:
+            for name, spec in dependencies.items():
+                if name in d:
+                    raise ValueError('dependency already exists')
+                d[name] = spec
+    return d
+
+
+def write_json(file, data):
+    with open(file, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+def read_toml(file):
+    with open(file, 'r') as f:
+        data = toml.load(f)
+    return data
 
 
 if __name__ == '__main__':
---
+++ version_spec.py
@@ -0,0 +1,77 @@
+class VersionSpec:
+    def __init__(self, ranges):
+        self.ranges = ranges
+
+    @staticmethod
+    def parse(pattern):
+        if isinstance(pattern ,list):
+            ranges = [parse_range(string) for string in pattern]
+        elif isinstance(pattern, str):
+            ranges = [parse_range(pattern)]
+        else:
+            raise ValueError('must be str or list')
+        return VersionSpec(ranges)
+
+    def __str__(self):
+        inf = float('inf')
+        if len(self.ranges) == 1 and self.ranges[0][0] == (0, 0, 0) and self.ranges[0][1] == (inf, inf, inf):
+            s = '*'
+        else:
+            s = ', '.join([range_to_str(r[0], r[1]) for r in self.ranges])
+        return s
+
+    def __contains__(self, version):
+        return False
+
+
+def parse_range(string):
+    if '-' in string:
+        lower, upper = string.split('-')
+    else:
+        lower = string
+        upper = string
+    lower = parse_version(lower, default=0)
+    upper = parse_version(upper, default=float('inf'))
+    rng = (lower, upper)
+    return rng
+
+
+def parse_version(string, default=None):
+    n = string.count('.')
+    if default is None and n != 2:
+        raise ValueError('if default is None a version number must be in the format x.y.z')
+    if default == float('inf') and string == '*':
+        major = default
+        minor = default
+        patch = default
+    else:
+        if n == 0:
+            major = int(string)
+            minor = default
+            patch = default
+        elif n == 1:
+            first, second = string.split('.')
+            major = int(first)
+            minor = int(second)
+            patch = default
+        elif n == 2:
+            first, second, third = string.split('.')
+            major = int(first)
+            minor = int(second)
+            patch = int(third)
+        else:
+            raise ValueError(f'invalid version: {string}')
+    version = (major, minor, patch)
+    return version
+
+
+def range_to_str(lower, upper):
+    if lower == upper:
+        s = version_to_str(lower)
+    else:
+        s = f'{version_to_str(lower)} - {version_to_str(upper)}'
+    return s
+
+
+def version_to_str(t):
+    return '.'.join(['*' if n == float('inf') else str(n) for n in t])
